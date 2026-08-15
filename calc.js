77894a015a23bbc0a42206be863ee5f4c9b9035e
Caio Augusto <caioguto05@gmail.com>
2026-08-14 22:13:15 -0300

    soma completo

--- calc.js
+++ calc.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const botaoAdd = document.getElementById('plus')
+
+function soma () {
+    const num1 =  Number(document.getElementById('n1').value)
+    const num2 = Number(document.getElementById('n2').value)
+    const result = document.getElementById('resultado')
+
+    result.textContent = num1 + num2 
+}
+
+botaoAdd.onclick = soma
