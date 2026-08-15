c1448afe89ce21649546223c74b0405a94f812d7
Caio Augusto <caioguto05@gmail.com>
2026-08-14 22:16:32 -0300

    Subtração completo

--- calc.js
+++ calc.js
@@ -11,3 +11,21 @@ function soma () {
 }
 
 botaoAdd.onclick = soma
+
+
+const botaoSub = document.getElementById('sub')
+
+function subtraçao () {
+    const num1 =  Number(document.getElementById('n1').value)
+    const num2 = Number(document.getElementById('n2').value)
+    const result = document.getElementById('resultado')
+
+    result.textContent = num1 - num2 
+}
+
+botaoSub.onclick = subtraçao
+
+
+
+
+
