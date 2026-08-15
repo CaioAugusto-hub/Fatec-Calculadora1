daff355ce2467739ea498f4101c67359bd7feb1d
Caio Augusto <caioguto05@gmail.com>
2026-08-14 22:23:33 -0300

    Projeto concluido

--- calc.js
+++ calc.js
@@ -38,3 +38,16 @@ function divisao () {
 
 botaoDiv.onclick = divisao
 
+
+const botaoMult = document.getElementById('mult')
+
+function multiplicacao () {
+    const num1 =  Number(document.getElementById('n1').value)
+    const num2 = Number(document.getElementById('n2').value)
+    const result = document.getElementById('resultado')
+
+    result.textContent = num1 * num2 
+}
+
+botaoMult.onclick = multiplicacao
+
